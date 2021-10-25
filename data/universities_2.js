79ad92d8fe72c56c22d5ef53b2f0d4d44ce5d22c
kapanadze <kapan.gio777@gmail.com>
2021-10-25 19:02:17 +0400

    version 1.0
    version 1.0

---
+++ data/universities_2.js
@@ -0,0 +1,22 @@
+var json_universities_2 = {
+    "type": "FeatureCollection",
+    "name": "universities_2",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+        { "type": "Feature", "properties": { "pk": 1, "university": "ივანე ჯავახიშვილის სახელობის თბილისის სახელმწიფო უნივერსიტეტი", "WEB": "https:\/\/www.tsu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.7787363, 41.709956, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 2, "university": "ილიას სახელმწიფო უნივერსიტეტი", "WEB": "https:\/\/iliauni.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.770775, 41.7089634, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 3, "university": "საქართველოს ტექნიკური უნივერსიტეტი", "WEB": "http:\/\/gtu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.778168, 41.7171519, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 4, "university": "გურამ თავართქილაძის სახელობის თბილისის სასწავლო უნივერსიტეტი", "WEB": "http:\/\/gttu.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.8216741, 41.7326039, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 5, "university": "სოხუმის სახელმწიფო უნივერსიტეტი", "WEB": "https:\/\/www.sou.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.7080554, 41.7210921, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 6, "university": "ბიზნესისა და ტექნოლოგიების უნივერსიტეტი", "WEB": "https:\/\/btu.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.7383023, 41.7067248, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 7, "university": "აკაკი წერეთლის სახელმწიფო უნივერსიტეტი", "WEB": "https:\/\/atsu.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [42.7106339, 42.2645213, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 8, "university": "საქართველოს ეროვნული უნივერსიტეტი", "WEB": "http:\/\/www.seu.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.8279249, 41.6900255, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 9, "university": "საქართველოს უნივერსიტეტი\n", "WEB": "https:\/\/www.ug.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.7792085, 41.724462, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 10, "university": "წმ. ტბელ აბუსერისძის სახელობის სასწავლო უნივერსიტეტი.", "WEB": "http:\/\/tbeli.ge\/" }, "geometry": { "type": "Point", "coordinates": [42.1290853, 41.6454853, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 11, "university": "ბათუმის შოთა რუსთაველის სახელმწიფო უნივერსიტეტი", "WEB": "https:\/\/www.bsu.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [41.6268116, 41.6498299, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 12, "university": "იაკობ გოგებაშვილის თელავის სახელმწიფო უნივერსიტეტი", "WEB": "http:\/\/tesau.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [45.4682286, 41.9102095, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 13, "university": "თბილისის სასულიერო აკადემია", "WEB": "http:\/\/www.tsas.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.8069084, 41.6953833, 0.0] } },
+        { "type": "Feature", "properties": { "pk": 14, "university": "საქართველოს აგრარული უნივერსიტეტი", "WEB": "https:\/\/agruni.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [44.767919212545422, 41.80646018078945, 0.0] } },
+        { "type": "Feature", "properties": { "pk": null, "university": "ქუთაისის უნივერსიტეტი", "WEB": "https:\/\/www.unik.edu.ge\/" }, "geometry": { "type": "Point", "coordinates": [42.704980215355697, 42.268393725259415, 0.0] } }
+    ]
+}
